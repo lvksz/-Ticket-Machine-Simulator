ab52add6de11b9fd1f0b464203a8103cfaabb4cb
ElizA001 <92113667+ElizA001@users.noreply.github.com>
2024-05-20 21:47:46 +0200

    Add basic Flask app with ticket prices

---
+++ app/app.py
@@ -0,0 +1,18 @@
+import random
+from flask import Flask
+
+app = Flask(__name__)
+
+TICKET_PRICES = {
+    'Bilet 20 minutowy': 4,
+    'Bilet 60 minutowy': 6,
+    'Bilet 24 godzinny': 10,
+    'Bilet 72 godzinny': 25
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
